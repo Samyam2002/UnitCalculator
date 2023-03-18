b9e9ec06e49011beec0485b291d685394863e265
Samyam2002 <ssamyam20@tbc.edu.np>
2023-03-18 15:32:08 +0545

    add comments

--- app/src/main/java/com/example/unitcalculator/MainActivity.kt
+++ app/src/main/java/com/example/unitcalculator/MainActivity.kt
@@ -7,6 +7,9 @@ import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+/**
+ * This activity allows the user to convert from ml to oz and vice versa
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +21,12 @@ class MainActivity : AppCompatActivity() {
         val inverse = findViewById<Switch>(R.id.inverse)
 
         button.setOnClickListener() {
+            //conversion from ml to oz
             val ml = inputValue.text.toString().toFloat()
             val oz = ml * 0.03381402
             outputValue.text = oz.toString()
 
+            //conversion from oz to ml
             if (inverse.isChecked) {
                 val oz = inputValue.text.toString().toFloat()
                 val ml = oz / 0.03381402
